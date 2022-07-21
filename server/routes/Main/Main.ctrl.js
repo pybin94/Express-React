e1743c1acff75e7566d08ada33b33924e93541cc
pybin94 <73331322+pybin94@users.noreply.github.com>
2022-07-21 20:12:06 +0900

    Delete server/routes/Main directory

--- server/routes/Main/Main.ctrl.js
+++
@@ -1,9 +0,0 @@
-import logger from "../../helper/LogHelper.js";
-// import pool from "../../helper/DBPool.js";
-import path from "path";
-
-const __dirname = path.resolve();
-
-export const main = async (req, res, next) => {
-    
-}
