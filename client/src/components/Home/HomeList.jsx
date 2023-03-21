d6748f1b2bd466183a6f0124ac8565af20f55514
pybin94 <73331322+pybin94@users.noreply.github.com>
2023-03-21 10:16:57 +0900

    Update HomeList.jsx

--- client/src/components/Home/HomeList.jsx
+++ client/src/components/Home/HomeList.jsx
@@ -8,16 +8,6 @@ const HomeList = () => {
 
     const navigate = useNavigate();
 
-  
-    let [betterzone, setBetterzone] = useState(["베터존", 0, 0, 0, 0, 0, 0]);
-    let [powersoft, setPowersoft] = useState(["파워소프트", 0, 0, 0, 0, 0, 0]);
-    let [allpick, setAllpick] = useState(["올픽", 0, 0, 0, 0, 0, 0]);
-    let [teamlemon, setTeamlemon] = useState(["팀레몬", 0, 0, 0, 0, 0, 0]);
-    let [outopowerball, setOutopowerball] = useState(["오파모", 0, 0, 0, 0, 0, 0]);
-    let [totoplus, setTotoplus] = useState(["토토플러스", 0, 0, 0, 0, 0, 0]);
-
-    let [siteList, setStieList] = useState([betterzone, powersoft, allpick, teamlemon, totoplus]);
-
     const handelAnalyticsApi = async (api, arrayStore, arrayState, setArrayState) => {
 
         arrayStore = [...arrayState];
@@ -33,10 +23,7 @@ const HomeList = () => {
         try {
             const response = await fetch(api, option);
             const res = await response.json();
-            console.log(res)
-            // for(let i = 0; i < 7; i++) {
-            //     week += res[i].enterances
-            // }
+
             res.forEach((v, i) => {
                 month += res[i].enterances
             })
@@ -54,43 +41,12 @@ const HomeList = () => {
     }
 
     useEffect(() => {
-        handelAnalyticsApi(url("analytics"), "betterzoneArray", betterzone, setBetterzone);
-        // handelAnalyticsApi(url("analytics"), "betterzoneArray", betterzone, setPowersoft);
-        // handelAnalyticsApi(url("analytics"), "betterzoneArray", betterzone, setAllpick);
-        // handelAnalyticsApi(url("analytics"), "betterzoneArray", betterzone, setBetterzone);
-        // handelAnalyticsApi(url("analytics"), "betterzoneArray", betterzone, setBetterzone);
-        setStieList([betterzone, powersoft, allpick, teamlemon, totoplus])
+
     }, [])
 
     return (
         <div className="home-list">
-            {
-                siteList.map((v, i) => {
-                    return (
-                        <div 
-                            className="home-list-container"
-                            key = {i}
-                            onClick={()=>{ 
-                                navigate(`/detail/${siteList[i][0]}`)
-                            }}
-                        >
-                            {
-                                siteList[i].map((v, j)=>{
-                                    return (
-                                        <div 
-                                            className="list-button"
-                                            key={j} 
-                                        >
-                                            {siteList[i][j]}
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                    )
-                })
-                
-            }
+           
         </div>
     )
 }
